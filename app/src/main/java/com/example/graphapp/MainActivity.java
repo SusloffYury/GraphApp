e1a33521cd075cfef9e033e169db56c322d7aa7f
SusloffYury <susloff.yury@gmail.com>
2019-07-09 19:13:11 +0300

    First commit

---
+++ app/src/main/java/com/example/graphapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.graphapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.FacebookSdk;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
